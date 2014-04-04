fefc1960c895e05553b10017b287f42972cf72e3
Shaps Mohsenin <shaps80@me.com>
2014-04-04 22:23:19 +0100

    Initial Commit

---
+++ iMessageStyleReveal/SPXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPXAppDelegate.h
+//  iMessageStyleReveal
+//
+//  Created by Shaps Mohsenin on 04/04/2014.
+//  Copyright (c) 2014 Snippex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iMessageStyleRevealTests/iMessageStyleRevealTests.m
@@ -0,0 +1,34 @@
+//
+//  iMessageStyleRevealTests.m
+//  iMessageStyleRevealTests
+//
+//  Created by Shaps Mohsenin on 04/04/2014.
+//  Copyright (c) 2014 Snippex. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iMessageStyleRevealTests : XCTestCase
+
+@end
+
+@implementation iMessageStyleRevealTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
